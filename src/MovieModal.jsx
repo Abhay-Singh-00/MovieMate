04c01254f7387caf17d8d811b206aeed6922f45d
Abhay-Singh-00 <abhayrathore002>
2025-04-19 21:11:42 +0530

    update

--- src/MovieModal.jsx
+++ src/MovieModal.jsx
@@ -8,22 +8,22 @@ const MovieModal = ({ isModalOpen, movie, modalClose }) => {
             isOpen={isModalOpen}
             onRequestClose={modalClose}
             contentLabel={movie.Title}
-            className="modal-container max-w-4xl sm:max-w-2xl mx-auto mt-12 sm:mt-16 p-6 bg-white rounded-3xl shadow-xl outline-none overflow-auto"
-            overlayClassName="overlay-fixed fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center"
+            className="max-w-full w-auto max-h-full mx-4 sm:mx-0 p-8 bg-white rounded-3xl shadow-xl outline-none overflow-auto"
+            overlayClassName="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center"
         >
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-8">
+            <div className="flex flex-col md:flex-row gap-8 md:max-w-5xl w-full">
                 {/* Movie Image */}
                 <img
                     src={movie.Poster}
                     alt={movie.Title}
-                    className="w-full sm:w-64 h-auto sm:h-96 rounded-xl shadow-2xl transform hover:scale-105 transition duration-300 ease-in-out"
+                    className="w-full md:w-64 h-96 object-cover rounded-xl shadow-2xl transition duration-300 ease-in-out"
                 />
 
                 {/* Movie Details */}
                 <div className="text-gray-800 space-y-4 text-lg flex flex-col justify-between">
                     <div>
-                        <h2 className="text-2xl sm:text-4xl font-extrabold text-center sm:text-left">{movie.Title}</h2>
-                        <p className="text-lg text-gray-600 text-center sm:text-left">📅 {movie.Year}</p>
+                        <h2 className="text-4xl font-extrabold">{movie.Title}</h2>
+                        <p className="text-lg text-gray-600">📅 {movie.Year}</p>
                     </div>
 
                     <div>
@@ -38,10 +38,10 @@ const MovieModal = ({ isModalOpen, movie, modalClose }) => {
             </div>
 
             {/* Close Button */}
-            <div className="flex justify-center sm:justify-end w-full mt-4">
+            <div className="flex justify-end w-full mt-4">
                 <button
                     onClick={modalClose}
-                    className="px-6 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition duration-200"
+                    className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition duration-200"
                 >
                     Close
                 </button>
